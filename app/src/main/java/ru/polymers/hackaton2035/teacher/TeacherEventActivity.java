e30edb76625c65b94edce015a0af68a80f20b416
Dmitry <dmitry.drobotov123@gmail.com>
2017-11-09 00:55:15 +0300

    Style graph

--- app/src/main/java/ru/polymers/hackaton2035/teacher/TeacherEventActivity.java
+++ app/src/main/java/ru/polymers/hackaton2035/teacher/TeacherEventActivity.java
@@ -1,14 +1,22 @@
 package ru.polymers.hackaton2035.teacher;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IValueFormatter;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,14 +36,9 @@ public class TeacherEventActivity extends AppCompatActivity {
     private BackendInterface backend = new MainBackend(new FrontendInterface() {
     
         @Override
-        public void setEventNames(Event[] events) {
-        
-        }
-    
+        public void setEventNames(Event[] events) {}
         @Override
-        public void setEvent(Event event) {
-        
-        }
+        public void setEvent(Event event) {}
     
         @Override
         public void setEntry(Entry entry) {
@@ -64,12 +67,53 @@ public class TeacherEventActivity extends AppCompatActivity {
         entries.add(new Entry(10f, 1f));
         entries.add(new Entry(15f, 0f));
         entries.add(new Entry(20f, 5f));
+        entries.add(new Entry(25f, 4f));
+        entries.add(new Entry(30f, 0f));
+        entries.add(new Entry(35f, 0f));
     
         LineDataSet dataSet = new LineDataSet(entries, "Активность");
         LineData lineData = new LineData(dataSet);
+        
+        styleChart(dataSet, lineData);
+        
         timeline.setData(lineData);
         timeline.invalidate(); // refresh
         
+    }
+    
+    private void styleChart(LineDataSet dataSet, LineData lineData) {
+        dataSet.setCircleColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        dataSet.setDrawCircleHole(false);
+        dataSet.setValueTextSize(15f);
+        
+        dataSet.setColor(ContextCompat.getColor(this, R.color.colorPrimary));
+    
+        IAxisValueFormatter formatter = (value, axis) -> String.valueOf((int) value);
+        IValueFormatter valueFormatter = (value, entry, dataSetIndex, viewPortHandler) -> String.valueOf((int) value);
+        lineData.setValueFormatter(valueFormatter);
+    
+        XAxis xAxis = timeline.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setValueFormatter(formatter);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawLabels(false);
+    
+        timeline.getAxisLeft().setDrawGridLines(false);
+        timeline.getAxisLeft().setEnabled(false);
+    
+        timeline.getAxisRight().setDrawGridLines(false);
+        timeline.getAxisRight().setEnabled(false);
+    
+        Description description = new Description();
+        description.setText("");
+        timeline.setDescription(description);
+        timeline.setHighlightPerDragEnabled(false);
+        timeline.setHighlightPerTapEnabled(false);
+    
+        timeline.setDrawBorders(false);
+        timeline.setGridBackgroundColor(ContextCompat.getColor(this, R.color.transparent));
+        
+        lineData.setValueTextColor(R.color.colorPrimary);
     }
 }
 
